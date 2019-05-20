0b7914867ecc9feceb5923e3e5f100f12011a70e
Vlado Cavric <vladocavric@gmail.com>
2019-05-20 16:31:36 +0200

    changed order of animation its better now

--- web/themes/start/dist/js/scripts.js
+++ web/themes/start/dist/js/scripts.js
@@ -1,22 +1,26 @@
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
+    const burger = document.querySelector('.c-nav-burger');
+    const nav = document.querySelector('.c-nav-ul');
+    const navLinks = document.querySelectorAll('.c-nav-ul li');
+    const burgerL1 = document.querySelector('.c-nav-burger-l1');
+    const burgerL2 = document.querySelector('.c-nav-burger-l2');
+    const burgerL3 = document.querySelector('.c-nav-burger-l3');
     
     burger.addEventListener('click',()=>{
-        nav.classList.toggle('nav-active');
+        nav.classList.toggle('c-nav-active');
+        burgerL1.classList.toggle('c-nav-burger-l1-x');
+        burgerL2.classList.toggle('c-nav-burger-l2-x');
+        burgerL3.classList.toggle('c-nav-burger-l3-x');
 
         navLinks.forEach((link, index) => {
-            link.style.animation = 'navLinksFade 0.5s ease forwards ${index / 7 + 3}s';
-           
+            link.style.animation = '2s ease forwards navLinksFade ';
+            
         });
         
     });
 
-   
-
-
-   
 }
 
 navSlide();
+
+//0.5s ease forwards ${index / 7 + 3}s
